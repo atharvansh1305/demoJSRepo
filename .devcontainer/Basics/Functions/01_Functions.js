849ab2f1eba0c0d3b80a9607c6f1870969d96ea8
atharvansh1305 <atharvansh1305@gmail.com>
2024-05-14 15:21:49 +0000

    commit again

--- .devcontainer/Basics/Functions/01_Functions.js
+++ .devcontainer/Basics/Functions/01_Functions.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+//console.log("Hello");
+
+//function myAdding(num1,num2){
+ //   return num1+num2;
+//}
+
+//const result=myAdding(10,20);
+//console.log("Result",result);
+
+ //resulting=(num1,num2)=>{
+   // return num1+num2;
+//}
+
+//console.log("Final Result : ",resulting(20,50));
+
+function validation(userName){
+    if(userName=== undefined){
+    console.log("Please Enter User Name");
+    return 
+    }
+    return `${userName} sucessfully log In`;
+}
+console.log(validation());
