2ef42c757ac5d12fc3499f7ee7fb749d5ea35ee4
atharvansh1305 <atharvansh1305@gmail.com>
2024-05-14 14:28:22 +0000

    commit

--- .devcontainer/Basics/01_ArraysFUnction.js
+++ .devcontainer/Basics/01_ArraysFUnction.js
@@ -1,5 +1,10 @@
 const  caps1=["Delhi","Lucknow","Jaipur"];
 const caps2=["Mumbai","Chennai","Culcutta"];
 
-caps1.concat(caps2);
-console.log(caps1);
+//const allcapital=caps1.concat(caps2);
+//console.log(allcapital);
+
+
+const myArraysNew=[1,2,3,["A","B",["a","b",[4,5,[7,8]]]]];
+const againArrays=myArraysNew.flat(4);
+console.log(againArrays);
